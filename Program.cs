0c8b210da158c672123fb203000c487f65486d95
TechNguyen <ndt13102003@gmail.com>
2023-05-24 16:46:19 +0700

    fisrt time c#

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+class Program
+{
+   public static void Main(string[] args)
+   {
+        Console.WriteLine("Hello, World!");
+   }
+}
